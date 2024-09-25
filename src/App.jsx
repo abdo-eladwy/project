fbbdd07b2da32ef6b8b0ef5aff97745f5274a5f4
abdo-eladwy <130322205+abdo-eladwy@users.noreply.github.com>
2024-09-25 11:04:02 +0200

    create project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import RegisterPage from "./SiteComponent/RegisterPage";
+function App() {
+  return (
+      
+
+        
+      <RegisterPage/>
+
+      
+
+); 
+{/* <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RegisterPage/>}></Route>
+      <Route path="/login" element={<LoginPage/>}></Route>
+      
+
+    </Routes>
+ 
+ </BrowserRouter>
+    */}
+    
+   
+
+ 
+}
+
+export default App;
---
+++ src/SiteComponent/RegisterPage.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+export default function RegisterPage() {
+  return (
+    <div className="register-page ">
+      
+      <div className="container col-11  col-lg-4   contents d-flex flex-column gap-5 justify-content-center align-content-center">
+        <h2> welcome!</h2>
+        <div className="inputs d-flex flex-column gap-3">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password " />
+          <input type="password" placeholder="Confirm Password" />
+        </div>
+        <button className="  text-white ">Sign Up</button>
+        <div className="  d-flex flex-column  flex-md-row  gap-md-3">
+          <p>Already have an account?</p>
+          <a href="#" >Sign in</a>
+        </div>
+      </div>
+    </div>
+  );
+}
